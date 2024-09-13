54ee2a86721a3bc4f248127c2ee2e9320e98430c
Larry Eitel <larry@eitel.com>
2024-09-13 08:42:05 -0600

    try pg on supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <slot></slot>
 <footer class="info">
 	<p>Double-click to edit a todo</p>
+
+
 	<p>
-		Ported from <a href="https://github.com/rocicorp/todo-wc"
+		<a href="https://github.com/LarryEitel/todo-replicache-sveltekit-supabase">todo-replicache-sveltekit-supabase</a> by Larry Eitel
+	</p>
+	<p>
+		Ported from <a href="https://github.com/isaacHagoel/todo-replicache-sveltekit">isaacHagoel/todo-replicache-sveltekit</a>
+	</p>
+	<p>
+		Which was ported from <a href="https://github.com/rocicorp/todo-wc"
 			>todo-wc (official replicache example)</a
 		>
-		and <a href="https://github.com/sveltejs/svelte-todomvc">svelte-todomvc</a> to Sveltekit by Isaac
-		Hagoel
 	</p>
 	<p>Not officially a part of <a href="http://todomvc.com">TodoMVC</a></p>
 	<p>
